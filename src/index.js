2e984324cfc2d59587bfd57dabf03c05341d85f5
Андрей Хавкунов <andrey.meg@mail.ru>
2021-10-11 22:58:43 +0300

    css loader config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import './styles/styles.css'
+
 document.querySelector('h1').innerText = 'Welcome to xavkunov.ru'
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 const config = {
     context: path.resolve(__dirname, 'src'),
@@ -11,12 +11,38 @@ const config = {
         filename: "[name].[contenthash].js",
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        watchFiles: {
+            paths: ['./'],
+            options: {
+                usePolling: false,
+            }
+        },
+        static: {
+            directory: path.join(__dirname, 'public'),
+            watch: true,
+        },
+        compress: true,
+        port: 3000,
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: "./index.html"
         }),
         new CleanWebpackPlugin()
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpeg|svg|gif)$/,
+                use: ['file-loader']
+            }
+        ]
+    }
 }
 
 module.exports = config
