241b6864b0f7f1628f9c680ed8c424a58a87dd35
Andrey Khavkunov <andrey.k@0hub.com>
2021-12-11 22:16:54 +0300

    draft i18n added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React, { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+
 import photo from './assets/photo.jpeg'
 import dream from './assets/dream.jpeg'
 
 const App: FC = () => {
+    const { t } = useTranslation()
+
     return (
         <main>
-            <h1>Добро пожаловать на мою страничку</h1>
+            <h1>{t('welcome')}</h1>
             <figure>
                 <img src={photo} alt="photo of me" />
-                <figcaption>это я</figcaption>
+                <figcaption>{t('me')}</figcaption>
             </figure>
             <figure>
                 <img src={dream} alt="photo of my dream" />
-                <figcaption>а это моя мечта</figcaption>
+                <figcaption>{t('dream')}</figcaption>
             </figure>
         </main>
     )
